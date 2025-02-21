8e5225cef9ef1fd8eb0523c4763e5128be8f867c
LuisS-byte <luis.sarmiento@catolica.edu.sv>
2025-02-20 18:37:56 -0600

    commit

---
+++ L01_NUMEROS_CARNETS/Controllers/CalificacionesController.cs
@@ -0,0 +1,114 @@
+﻿using L01_NUMEROS_CARNETS.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace L01_NUMEROS_CARNETS.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalificacionesController : ControllerBase
+    {
+
+
+        private readonly AppDbContext _context;
+        public CalificacionesController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        [Route("GetAll")]
+        public IActionResult Get()
+        {
+            var listadoCalificaciones = (from c in _context.Calificaciones
+                                         join p in _context.Publicaciones on c.PublicacionId equals p.PublicacionId
+                                         join u in _context.Usuarios on c.UsuarioId equals u.UsuarioId
+                                         select new
+                                         {
+                                             CalificacionId = c.CalificacionId,
+                                             Publicacion = p.Titulo,
+                                             Usuario = u.Nombre + " " + u.Apellido,
+                                             Puntaje = c.Calificacion
+                                         }).ToList();
+
+            if (listadoCalificaciones.Count == 0)
+            {
+                return NotFound();
+            }
+
+            return Ok(listadoCalificaciones);
+        }
+
+
+        [HttpGet]
+        [Route("FiltrarPorPublicacion/{publicacionId}")]
+        public IActionResult FiltrarPorPublicacion(int publicacionId)
+        {
+            var calificaciones = (from c in _context.Calificaciones
+                                  join p in _context.Publicaciones on c.PublicacionId equals p.PublicacionId
+                                  join u in _context.Usuarios on c.UsuarioId equals u.UsuarioId
+                                  where c.PublicacionId == publicacionId
+                                  select new
+                                  {
+                                      CalificacionId = c.CalificacionId,
+                                      Publicacion = p.Titulo,
+                                      Usuario = u.Nombre + " " + u.Apellido,
+                                      Puntaje = c.Calificacion
+                                  }).ToList();
+
+            if (calificaciones.Count == 0)
+            {
+                return NotFound();
+            }
+
+            return Ok(calificaciones);
+        }
+
+
+        [HttpPost]
+        [Route("Create")]
+        public IActionResult Post([FromBody] Calificacione nuevaCalificacion)
+        {
+            _context.Calificaciones.Add(nuevaCalificacion);
+            _context.SaveChanges();
+            return Ok(nuevaCalificacion);
+        }
+
+
+        [HttpPut]
+        [Route("Update/{id}")]
+        public IActionResult Put(int id, [FromBody] Calificacione calificacionActualizada)
+        {
+            var calificacion = _context.Calificaciones.FirstOrDefault(c => c.CalificacionId == id);
+            if (calificacion == null)
+            {
+                return NotFound();
+            }
+
+            calificacion.PublicacionId = calificacionActualizada.PublicacionId;
+            calificacion.UsuarioId = calificacionActualizada.UsuarioId;
+            calificacion.Calificacion = calificacionActualizada.Calificacion;
+
+            _context.SaveChanges();
+            return Ok(calificacion);
+        }
+
+
+        [HttpDelete]
+        [Route("Delete/{id}")]
+        public IActionResult Delete(int id)
+        {
+            var calificacion = _context.Calificaciones.FirstOrDefault(c => c.CalificacionId == id);
+            if (calificacion == null)
+            {
+                return NotFound();
+            }
+
+            _context.Calificaciones.Remove(calificacion);
+            _context.SaveChanges();
+            return Ok();
+        }
+    }
+
+}
---
+++ L01_NUMEROS_CARNETS/Controllers/ComentariosController.cs
@@ -0,0 +1,98 @@
+﻿using L01_NUMEROS_CARNETS.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace L01_NUMEROS_CARNETS.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ComentariosController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        public ComentariosController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        [Route("ListaComentarios")]
+        public IActionResult ListaComentarios()
+        {
+            var comentarios = (from c in _context.Comentarios
+                               select c).ToList();
+            return Ok(comentarios);
+        }
+
+
+        [HttpPost]
+        [Route("CrearComentario")]
+        public IActionResult CrearComentario([FromBody] Comentario comentario)
+        {
+            //el id no es necesario
+            //lo hicismo asi ya que nos daba error 
+            /*
+             SqlException: No se puede insertar un valor explícito en la columna de identidad de la tabla 'comentarios' cuando IDENTITY_INSERT es OFF.
+            */
+            // y no queriamos modificar la base de datos ni las propiedades de ellas
+            //por esa manera lo hicimos de la siguiente manera
+            _context.Comentarios.Add(new Comentario
+            {
+                PublicacionId = comentario.PublicacionId,
+                Comentario1 = comentario.Comentario1,
+                UsuarioId = comentario.UsuarioId
+            });
+
+            _context.SaveChanges();
+            return Ok(comentario);
+        }
+
+
+        [HttpPut]
+        [Route("UpdateComentario/{id}")]
+        public IActionResult UpdateComentario(int id, [FromBody] Comentario comentarioActualizado)
+        {
+            var comentario = (from c in _context.Comentarios
+                              where c.CometarioId == id
+                              select c).FirstOrDefault();
+            if (comentario == null) return NotFound();
+
+            comentario.PublicacionId = comentarioActualizado.PublicacionId ?? comentario.PublicacionId;
+            comentario.Comentario1 = comentarioActualizado.Comentario1 ?? comentario.Comentario1;
+            comentario.UsuarioId = comentarioActualizado.UsuarioId ?? comentario.UsuarioId;
+
+            _context.SaveChanges();
+            return Ok(comentario);
+        }
+
+        [HttpDelete]
+        [Route("DeleteComentario/{id}")]
+        public IActionResult DeleteComentario(int id)
+        {
+            var comentario = (from c in _context.Comentarios
+                              where c.CometarioId == id
+                              select c).FirstOrDefault();
+            if (comentario == null) return NotFound();
+
+            _context.Comentarios.Remove(comentario);
+            _context.SaveChanges();
+            return Ok(200);
+        }
+
+
+
+
+
+
+
+        [HttpGet]
+        [Route("GetComentariosPorUsuario/{usuarioId}")]
+        public IActionResult GetComentariosPorUsuario(int usuarioId)
+        {
+            var comentarios = (from c in _context.Comentarios
+                               where c.UsuarioId == usuarioId
+                               select c).ToList();
+            return Ok(comentarios);
+        }
+    }
+}
